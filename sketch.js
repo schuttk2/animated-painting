8d4a26e5db39662a39fef8821e01a63e8890d73b
Kiley Schutte <schuttk2@wwu.edu>
2024-01-29 18:05:46 -0800

    did a little tinkering

--- sketch.js
+++ sketch.js
@@ -7,10 +7,11 @@ function setup() {
  
  function draw() {
   
-  r = map(mouseX, 0, windowWidth, 225, 0);
-  g = map(mouseX, windowWidth, 0, 130, 0);
-  s = map(mouseY, 0, windowHeight, 80, 150);
-  strokeWeight(1);
+  r = map(mouseX, 0, windowWidth, 0, 255);
+  g = map(mouseX, windowWidth, 0, 0, 255);
+  s = map(mouseY, 0, windowHeight, 80, 200);
+  strokeWeight(3);
+  
   //red triangle
    fill(178-r, 30 -g, 10);
    quad(width-(s/5), 0+(s/5), width-(s/5), height-(s/5), 500+(s/5), height-(s/5), 220+(s/5), 375-(s/5));
@@ -48,44 +49,4 @@ function setup() {
    //yellow triangle
    fill(210+r, 161+g, 69);
    triangle(0+(s/5), height-(s/5), 140-(s/5), 476-(s/5), 0+(s/5), 320+(s/5));
-  
-
-
-   /*//red triangle
-   fill(178, 35, 19);
-   quad(width, 0, width, height, 500, height, 250, 375);
- 
-   //bottom gray triangle
-   fill(175, 180, 186);
-   triangle(250, 375, 500, height, 250, height);
- 
-   //top middle triangle
-   fill(209, 202, 192);
-   quad(250, 375, 250, 0, 315, 0, 460, 150);
- 
-   //top right triangle
-   fill(108, 98, 99);
-   triangle(width, 0, 460, 150, 315, 0);
- 
-   //black triangle
-   fill(44, 40, 39);
-   beginShape();
-   vertex(250, height);
-   vertex(250, 0);
-   vertex(225, 0);
-   vertex(0, 225);
-   vertex(0, 350);
-   endShape(CLOSE);
- 
-   //top right triangle
-   fill(168, 172, 181);
-   triangle(0, 0, 225, 0, 0, 225);
- 
-   //bottom white triangle
-   fill(223, 216, 208);
-   triangle(250, height, 125, 475, 0, height);
- 
-   //yellow triangle
-   fill(210, 161, 69);
-   triangle(0, height, 125, 475, 0, 350);*/
  }
